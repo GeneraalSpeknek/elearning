40bad53b2537acd612f2b742d328caad855d5bd7
vijfe <vijfe@DESKTOP-6CLG66U>
2016-11-21 12:33:50 +0100

    Add project files.

---
+++ Elearning_wpf/Dbs_Conn.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using MySql.Data.MySqlClient;
+
+namespace Elearning_wpf
+{
+    class Dbs_Conn
+    {
+        string strcnn = "Server=localhost;Database=elearning;uid=root;pwd=;";
+
+        public DataTable GetVakken()
+        {
+            MySqlConnection cnn = new MySqlConnection(strcnn);
+            cnn.Open();
+            MySqlCommand cmd = new MySqlCommand("SELECT * FROM vak",cnn);
+            MySqlDataReader rdr = cmd.ExecuteReader();
+            DataTable tbl = new DataTable();
+            tbl.Load(rdr);
+            cnn.Close();
+            return tbl;
+        }
+
+        public void AddVak(string vak)
+        {
+            MySqlConnection cnn = new MySqlConnection(strcnn);
+            cnn.Open();
+            MySqlCommand cmd = new MySqlCommand("INSERT INTO vak (naam) VALUES (@vak)", cnn);
+            cmd.Parameters.AddWithValue("@vak",vak);
+
+            cmd.ExecuteNonQuery();
+            cnn.Close();
+        }
+
+        public void ChangeVak(string IdVak, string vak)
+        {
+            MySqlConnection cnn = new MySqlConnection(strcnn);
+            cnn.Open();
+            MySqlCommand cmd = new MySqlCommand("UPDATE vak SET naam = @vaknaam WHERE id = @Id", cnn);
+            cmd.Parameters.AddWithValue("@vaknaam", vak);
+            cmd.Parameters.AddWithValue("@Id", IdVak);
+            cmd.ExecuteNonQuery();
+            cnn.Close();
+        }
+    }
+}
---
+++ Elearning_wpf/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Elearning_wpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string IdVak="";
+
+        struct Vakken
+        {
+            public string Id { get; set; }
+            public string VakNaam { get; set; }
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            PopulateLb();
+        }
+
+        private void PopulateLb()
+        {
+            DataTable dtVakken = new Dbs_Conn().GetVakken();
+            List<Vakken> lstVakken = new List<Vakken>();
+
+            foreach (DataRow drVakken in dtVakken.Rows)
+            {
+                lstVakken.Add(new Vakken() {Id = drVakken[0].ToString(), VakNaam = drVakken[1].ToString() });
+            }
+            lbVakken.ItemsSource = lstVakken;
+        }
+
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+            //voeg inhoud van textbox toe aan database
+            new Dbs_Conn().AddVak(tbVak.Text);
+            PopulateLb();
+            tbVak.Text = "";
+        }
+
+        private void btChangeVak_Click(object sender, RoutedEventArgs e)
+        {
+            new Dbs_Conn().ChangeVak(IdVak, tbVak.Text);
+            PopulateLb();
+            tbVak.Text = "";
+        }
+
+        private void lbVakken_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            IdVak = ((Vakken)(lbVakken.SelectedItem)).Id;
+            tbVak.Text = ((Vakken)(lbVakken.SelectedItem)).VakNaam;
+
+        }
+    }
+}
